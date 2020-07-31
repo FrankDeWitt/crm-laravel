a17b7d84bf5309b22b3046ddb48771033f19ef5e
Fran J Salvador <fran.j9.sc@gmail.com>
2020-07-31 11:01:30 +0200

    Name change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CRM
                 </div>
 
                 <div class="links">
